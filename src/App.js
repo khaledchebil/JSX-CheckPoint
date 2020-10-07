a455cd83cf870d85cee6a7aa5396cd4b8a412830
khaledchebil <70965320+khaledchebil@users.noreply.github.com>
2020-10-07 15:48:49 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import pic1 from './asset/pic1.jpg';
 import './style.css';
 
 function App() {
+  const mystyle = {
+    color: "black",
+    backgroundColor: "yellow",
+    padding: "10px",
+    fontFamily: "Arial",
+      };
   return (
     <div className="App">
 
 <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
-<h1 className="title red">Anything Here</h1>
+<h1 className="title red"   >Anything Here</h1>
+<h4 style={mystyle}> this paragraph to try to use another way of style</h4>
+<br />
 <img src="./images/pic2.jpg" alt="this is "  width="320" height="240" />
+<br />
 <img src={pic1} alt='this is an 2' width="320" height="240" className="secpic"/>
 </div>
 
